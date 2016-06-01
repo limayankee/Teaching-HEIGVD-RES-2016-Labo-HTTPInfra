03633fd99b6f736435f32d7f3cf25ba14db163b1
Julien Leroy <julien.leroy@limayankee.com>
2016-06-01 14:28:02 +0200

    fini

--- Docker/Node/scr/index.js
+++ Docker/Node/scr/index.js
@@ -1,4 +1,19 @@
 const Chance = require('chance');
-const chance = new Chance;
+const express = require('express');
+var starwars = require('starwars');
 
-console.log(chance.name());
+
+const chance = new Chance;
+const app = new express;
+
+
+
+app.get('/', function(req, res){
+  const number = chance.integer({min: 0, max: 2})
+
+  res.send(starwars());
+});
+
+app.listen(8080, function(){
+  console.log('Accepting HTTP requests on port 8080');
+});
